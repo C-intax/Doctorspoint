c2e84da8aa9a22d1aeace27232cc85b4eb8fcfbd
Mohammad Yasin Arafat <arafat102875@gmail.com>
2019-11-05 21:00:30 +0800

    SQL with Dummy Data

--- doctorspoint.sql
+++ doctorspoint.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 03, 2019 at 02:36 PM
+-- Generation Time: Nov 05, 2019 at 01:59 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 5.6.39
 
@@ -107,7 +107,7 @@ CREATE TABLE `doctors_assistant` (
 
 CREATE TABLE `doctors_chamber` (
   `id` int(11) NOT NULL,
-  `docters_id` int(11) DEFAULT NULL,
+  `doctor_id` int(11) DEFAULT NULL,
   `name` text,
   `location` text,
   `contact_number` text,
@@ -115,6 +115,17 @@ CREATE TABLE `doctors_chamber` (
   `end_time` varchar(50) DEFAULT NULL,
   `per_patient_interval` int(5) DEFAULT '10'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctors_chamber`
+--
+
+INSERT INTO `doctors_chamber` (`id`, `doctor_id`, `name`, `location`, `contact_number`, `start_time`, `end_time`, `per_patient_interval`) VALUES
+(1, 1, 'Lab Aid Diagnostic', '12/4B Dhanmondi, Dhaka', '+8801824819966', '6:30 PM', '9:30 PM', 10),
+(2, 1, 'Ibna Sina Lab', '15/6 Kakrail Dhaka', '+8801716237654', '4:30 PM', '6:30 PM', 10),
+(3, 1, 'Khan Medical Centre', 'Old Jatrabari, Khetpara, Dhaka', '+01813764534', '9:30 AM', '11:30 AM', 10),
+(4, 2, 'Apollo Diagnostic Centre', 'Block A, Bashundhora R/A, Dhaka', '+8801823432678', '10:30 AM', '12:30 PM', 10),
+(5, 2, 'Cresent Lab Ltd.', 'Sector 13, Uttara, Dhaka', '+88017346373928\r\n', '4:30 PM', '8:30 PM', 10);
 
 -- --------------------------------------------------------
 
@@ -128,6 +139,16 @@ CREATE TABLE `doctors_degree` (
   `institute` text,
   `degree_name` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctors_degree`
+--
+
+INSERT INTO `doctors_degree` (`id`, `doctor_id`, `institute`, `degree_name`) VALUES
+(1, 1, 'Dhaka Medical College', 'Bachelor of Medicine MBBS'),
+(2, 1, 'Sher e Bangla Medical College', 'FCPS'),
+(3, 2, 'Bangabondhu Sheukh Mujib Medical College', 'MBBS'),
+(4, 2, 'Sir Solimullah Medical College', 'FCPS');
 
 -- --------------------------------------------------------
 
@@ -150,6 +171,14 @@ CREATE TABLE `doctors_details` (
   `doctors_reg_no` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `doctors_details`
+--
+
+INSERT INTO `doctors_details` (`user_id`, `email`, `password`, `name`, `user_type`, `is_login`, `Is_activated`, `isVerified`, `password_change_date`, `previous_password`, `doctors_domain_id`, `doctors_reg_no`) VALUES
+(1, 'dummy_doctor_1@doctorspoint.com', 'dummydoctor_1', 'Dr. Faisal Hasan', 1, 0, 1, 1, NULL, NULL, '1', 'REG001'),
+(2, 'dummy_dotor_2@doctorspoint.com', 'dummydoctor_2', 'Dr. Ferdous Hasan', 1, 0, 1, 1, NULL, NULL, '2', 'REG002');
+
 -- --------------------------------------------------------
 
 --
@@ -161,6 +190,15 @@ CREATE TABLE `doctors_domain` (
   `domain_name` varchar(150) DEFAULT NULL,
   `description` longtext
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctors_domain`
+--
+
+INSERT INTO `doctors_domain` (`id`, `domain_name`, `description`) VALUES
+(1, 'Neuro Sergeon', 'Dummy 1 Domain'),
+(2, 'Medicine', 'Dummy 2 Domain'),
+(3, 'Darmatologyst', 'Dummy 3 Domain');
 
 -- --------------------------------------------------------
 
@@ -365,25 +403,25 @@ ALTER TABLE `doctors_assistant`
 -- AUTO_INCREMENT for table `doctors_chamber`
 --
 ALTER TABLE `doctors_chamber`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `doctors_degree`
 --
 ALTER TABLE `doctors_degree`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `doctors_details`
 --
 ALTER TABLE `doctors_details`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'user_id';
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'user_id', AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `doctors_domain`
 --
 ALTER TABLE `doctors_domain`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `patient_details`
